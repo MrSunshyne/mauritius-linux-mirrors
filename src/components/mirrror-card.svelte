d2cc03f03c130e4af9f66aee9eb44648820e4ed7
Sandeep Ramgolam <sandeep@ramgolam.com>
2023-03-19 12:52:32 +0400

    style fixes to the cards

--- src/components/mirrror-card.svelte
+++ src/components/mirrror-card.svelte
@@ -3,7 +3,7 @@
     export let mirror: MirrorDetail;
 </script>
 
-<div class="flex flex-col py-4 px-6 items-center gap-6 bg-black/40 rounded backdrop-blur-sm">
+<div class="flex flex-col py-4 px-6 items-center gap-6 bg-white/10 rounded-2xl backdrop-blur-sm">
     <div>
         <h2 class="text-2xl">{mirror.distro}</h2>
     </div>
@@ -11,7 +11,7 @@
         <img src={mirror.logo} alt="" class="block w-full h-full">
     </div>
     <div class="flex w-full justify-between">
-        <a class="block font-bold text-blue-700 hover:text-blue-600" href={mirror.http}> HTTP </a>
-        <a class="block font-bold text-green-700 hover:text-green-600" href={mirror.https}> HTTPS </a>
+        <a class="block font-bold  text-blue-500 hover:text-blue-400" href={mirror.http}> HTTP </a>
+        <a class="block font-bold text-green-500 hover:text-green-400" href={mirror.https}> HTTPS </a>
     </div>
 </div>
